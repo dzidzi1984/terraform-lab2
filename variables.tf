1bfa47567695fcde436ca856fe4ccc24b6bd3bf8
komla <romgbet@gmail.com>
2023-04-09 00:40:30 -0500

    terraform code

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "region_name" {
+  type    = string
+  default = "us-east-1"
+}
+variable "instance_type" {
+  description = "this is the instance"
+  type        = string
+  default     = "t2.nano"
+}
+variable "env" {
+    default = "dev"
+  
+}
+variable "name" {
+    default = "dev-jenkins-server"
+}                                
+            
